6c171a96e8256d0dbb693d1e54371e65a89f6da8
gholamihosni <gholamihosni@gmail.com>
2023-03-16 19:39:53 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+/**
+ * main - function that prints a given input within printf
+ * Return: 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
