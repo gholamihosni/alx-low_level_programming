0ae6b48e429c1499c4f21b518dceaa5d90d22436
gholamihosni <gholamihosni@gmail.com>
2023-03-16 19:45:47 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-/**
- * main - function that prints a given input within printf
- * Return: 0 (Success)
- */
 #include <stdio.h>
+/**
+ *main -entry point
+ *Return: always 0 if the program executed successfully
+*/
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
 }
