9f5963d97ceac5aee6ae60144e1cade963695098
gholamihosni <gholamihosni@gmail.com>
2023-03-19 17:54:05 -0700

    less material you need

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - main function to print combination of two numbers
+ *
+ * Returns: always 0 on success
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	while (i <= '9')
+	{
+		while (j <= '9')
+		{
+			if (!(i > j) || i == j)
+			{
+				putchar(i);
+				putchar(j);
+				if (i == '8' && j == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			j++;
+		}
+		j = '0';
+		i++;
+	}
+return (0);
+}
