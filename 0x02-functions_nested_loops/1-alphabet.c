ce18a4de4ae34bc883147e7068f2663cfc9395e0
gholamihosni <gholamihosni@gmail.com>
2023-03-21 19:38:00 -0700

    alphaabet insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - main function
+*
+* Return: Always 0
+*/
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar(i);
+	_putchar('\n');
+
+}
