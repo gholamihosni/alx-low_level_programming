77a1790a14aaf351e84974a79fe97e1f7dc95b89
gholamihosni <gholamihosni@gmail.com>
2023-03-22 18:12:46 -0700

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ *
+ * @a: para 1
+ * @b: para 2
+ * Return: always 0
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * times_table - print the 9 time table starting with 0
+ *
+ * Return: always 0
+ */
+
+void times_table(void)
+{
+
+
+}
